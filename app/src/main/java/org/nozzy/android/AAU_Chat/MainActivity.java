d2ee9362daa526474707cb93f994dd1c91314687
Vaiders <krisjanisvaiders@gmail.com>
2018-05-07 12:20:11 +0200

    Merged Burger menu

--- app/src/main/java/org/nozzy/android/AAU_Chat/MainActivity.java
+++ app/src/main/java/org/nozzy/android/AAU_Chat/MainActivity.java
@@ -59,13 +59,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         getSupportActionBar().setTitle("AAU Chat");
 
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        NavigationView navigationView = findViewById(R.id.nav_view);
 
         navigationView.setNavigationItemSelectedListener(this);
 
