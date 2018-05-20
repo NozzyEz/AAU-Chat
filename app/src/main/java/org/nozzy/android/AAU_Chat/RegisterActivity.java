027f73ef14145988ddec7d1e8ec2d1f38e13ca52
catalinabarbulescu <catalina.barbulescu@yahoo.com>
2018-05-20 16:04:38 +0300

    ..

--- app/src/main/java/org/nozzy/android/AAU_Chat/RegisterActivity.java
+++ app/src/main/java/org/nozzy/android/AAU_Chat/RegisterActivity.java
@@ -3,13 +3,16 @@ package org.nozzy.android.AAU_Chat;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.design.widget.TextInputEditText;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -37,6 +40,12 @@ public class RegisterActivity extends AppCompatActivity {
     private TextInputLayout mPassword;
     private Button mCreateBtn;
     private Toolbar mToolbar;
+    private Spinner mSemesterSpinner;
+    private Spinner mCoursesSpinner;
+    private TextInputEditText mRepeatPassword;
+    private ArrayAdapter<CharSequence> mCharSequenceArrayAdapterCourses;
+    private ArrayAdapter<CharSequence> mCharSequenceArrayAdapterSemester;
+
 
     private ProgressDialog mRegProgress;
 
